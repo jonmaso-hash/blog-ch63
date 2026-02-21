ee14189e1bc4454339b121a89aebd964ba9e3e8e
jonmaso-hash <jonmaso@gmail.com>
2026-02-20 16:53:57 -0800

    assignment 3

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    # Fields
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=256)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.DO_NOTHING
+    )
+
+    #dunder methods or double underscor methods or magic methods in python
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+
+    # Redirect after create/update
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[self.id])
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+
+from django.views.generic import (
+    ListView,
+    DeleteView,
+    DetailView,
+    CreateView,
+    UpdateView,
+)
+from .models import Post
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class PostListView(ListView):  # GET Request -> List
+    # Template_name attribute renders a specific html file
+    template_name = "posts/list.html"
+    
+    # model attribute lets Django know from which model (Table) we want to retrieve the data
+    model = Post
+    
+    # context_object_name attribute is used to change the variable name inside the template
+    context_object_name = "posts"
+
+class PostDetailView(DetailView): #GET Request -> Object
+    template_name = "posts/detail.html"
+    model = Post
+    context_object_name = "single_post"
+
+class PostCreateView(CreateView): #Post request -> form (HTML)
+    template_name = "posts/new.html"
+    model = Post
+    #fields attribute is a list that allow us to enable/disable the inputs to render in the html
+    fields = ["title", "subtitle", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = User.objects.last()
+        print(form.instance)
+        print(form)
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView): #POST Request -> filled form (HTML)
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView): #POST Request 
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("post_list")
